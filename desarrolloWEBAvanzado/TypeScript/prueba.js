adfa7030483be3f6e7da8d9592f8e63c30a61ac6
domingoLopezOller <domingolopez.instituto@gmail.com>
2023-04-24 23:33:31 +0200

    javascript intro

--- desarrolloWEBAvanzado/TypeScript/prueba.js
+++ desarrolloWEBAvanzado/TypeScript/prueba.js
@@ -1,16 +1,16 @@
 function datosPersona(person) {
-    return `La persona se llama ${person.name}, tiene ${person.age} años y mide ${person.height} cm`;
+    return "La persona se llama ".concat(person.name, ", tiene ").concat(person.age, " a\u00F1os y mide ").concat(person.height, " cm");
 }
-let persona = {
+var persona = {
     name: 'John Doe',
     age: 25,
     height: 170,
 };
 console.log(datosPersona(persona));
-const array = [1, 2, 3, 4];
-console.log(`La suma de los elementos del array es: ${array.reduce((a, b) => a + b)}`);
+var array = [1, 2, 3, 4];
+console.log("La suma de los elementos del array es: ".concat(array.reduce(function (a, b) { return a + b; })));
 var stringsearch = "o", str = "Texto donde buscar";
-let count = 0;
-for (let i = 0; i < str.length; count += +(stringsearch === str[i++]))
+var count = 0;
+for (var i = 0; i < str.length; count += +(stringsearch === str[i++]))
     ;
 console.log(count);
---
+++ desarrolloWEBAvanzado/javascript/prueba.js
@@ -0,0 +1,86 @@
+// let i=0;
+// for (i=0;i<10;i++){
+//     console.log(i);
+// }
+// console.log(i+3);
+// let j="hola";
+
+// const PI =3.141592;
+// let radio=3;
+// console.log(2*PI*radio);
+
+
+// const IVA =21;
+// let radioCirculo;
+// let ladoPoligonoRectoMayor;
+// let lado_poligono_recto_mayor;
+
+
+// // comentarios
+// /*
+// adsfadsf
+// asdfasd
+// asdfadsf
+// */
+
+// //Salida por pantalla/consola
+// console.log("HOla mundo");
+// document.write("<h1>Texto generado desde Javascipt</h1>");
+// // alert("Alerta de fallo en JavaScript");
+// // window.print();
+
+
+// //Entrada por teclado
+// let numero =prompt("Introduce un número");
+// document.write("El número introducido es: "+numero);
+// confirm("Pulsar para morir");
+
+//Código de if-else
+// let edad;
+// edad = Number((prompt("Introduce tu edad: ")));
+// if (isNaN(edad)){
+//     console.log("Lo que has introducido no es un número");
+// }
+//   else if (edad < 18) {
+// 	console.log("Tienes menos de 18 años.");
+// } else if (edad >= 18 && edad <= 21) {
+// 	console.log("Tenes entre 18 y 21 años de edad.");
+// } else {
+// 	console.log("Tiene más de 21 años.");
+// }
+
+//Código switch
+// let expr='+';
+// switch (expr) {
+//     case '+':
+//       console.log('Estás haciendo la suma');
+//       break;
+//     case 'Manzanas':
+//     case 'Platanos':
+//       console.log('El kilogramo cuesta $0.48.');
+//       break;
+//     default:
+//       console.log('Lo lamentamos, no disponemos de ' + expr + '.');
+//   }
+
+//Composición de texto
+console.log("Hola MUndo");
+let nombre="Domingo López Oller";
+let edad=37;
+console.log("¡Hola, "+nombre);
+console.log("¡Hola, "+nombre+ " tienes "+edad+ " años");
+
+//Versión ES6
+console.log(`¡Hola, ${nombre} tienes ${edad} años`);
+
+//Operador Elvis
+let valor=3;
+if(valor >=5){
+    console.log("Estás aprobado");
+}
+else{
+     console.log("Estás suspenso");
+}
+
+let resultado= valor>=5 ? "Estas aprobado": "Estás suspenso";
+console.log(resultado);

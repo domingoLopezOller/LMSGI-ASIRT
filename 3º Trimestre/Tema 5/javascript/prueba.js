b0787435279b3cce62608890a55b244131620cb6
Jfx2m <jfabmar2109@iesalbox.com>
2023-04-24 20:44:57 +0200

    24/04

---
+++ 3º Trimestre/Tema 5/javascript/prueba.js
@@ -0,0 +1,14 @@
+var i;
+for (i=0; i<10; i++){
+console.log(i);
+}
+console.log(i + 3);
+
+//Salida por pantalla/consola
+console.log("Hola Mundo");
+document.write("<h1>Texto Hecho desde javascript</h1>")
+//alert ("Alerta");
+
+//Entrada por taclado
+let n=prompt("Introduce un numero")
+document.write("El numero es: "+n)

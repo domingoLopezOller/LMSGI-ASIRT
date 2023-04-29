9ddad942d05946e6137156e51408bab6888e527d
lfvalcar <luisfer998845@gmail.com>
2023-04-30 01:19:08 +0200

    Tarea RWD terminada

---
+++ Tema5DesarrolloAvanzadoWeb/Practicas/prueba.js
@@ -0,0 +1,69 @@
+let i;
+for (i=0; i<10; i++){
+console.log(i);
+}
+console.log(i + 3 ); //devuelve 13
+let j="hola";
+console.log(i);
+
+const PI =3.141592
+
+let radio=3
+console.log(2*PI*radio)
+
+//Salida
+// console.log("Hola mundo");
+document.write("<h1>Texto generado desde javascript</h1>");
+//alert("Alerta de fallo");
+// window.print();
+
+
+//Entrada por teclado
+// let numero = prompt("Introduce un numero");
+// document.write("El numero introducido es: " + numero )
+// confirm("Pulsa para morir")
+
+// let edad;
+// edad = Number((prompt("Introduce tu edad: ")));
+// if(isNaN(edad)){
+//     console.log("Lo que has introducido no es un numero")
+// }
+// else if (edad < 18) {
+// console.log("Tienes menos de 18 años.");
+// } else if (edad >= 18 && edad <= 21) {
+// console.log("Tenes entre 18 y 21 años de edad.");
+// } else {
+// console.log("Tiene más de 21 años.");
+// }
+
+// let expr='Naranjas';
+// switch (expr) {
+//     case 'Naranjas':
+//     console.log('El kilogramo de naranjas cuesta $0.59.');
+//     break;
+//     case 'Manzanas':
+//     case 'Platanos':
+//     console.log('El kilogramo cuesta $0.48.');
+//     break;
+//     default:
+//     console.log('Lo lamentamos, no disponemos de ' + expr + '.');
+//     }
+
+console.log("Hola Mundo");
+let nombre="Luis Fernando Valverde";
+console.log("Hola, " + nombre);
+let edad=37;
+console.log("!Hola, " + nombre + " tienes " + edad + " años");
+
+console.log(`!Hola, ${nombre} tienes ${edad} años`);
+
+let valor=3;
+if(valor>5){
+    console.log("Estas aprobado");
+}else{
+    console.log("Estas suspenso");
+}
+
+let resultado=valor>=5 ? "Estas aprobado": "Estas suspenso";
+console.log(resultado);
+

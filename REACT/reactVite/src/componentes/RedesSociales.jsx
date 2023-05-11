59247bc01c7ed6d0ab67e82659e971420e42c7be
domingoLopezOller <domingolopez.instituto@gmail.com>
2023-05-11 20:10:22 +0200

    funciones parámetros REACT

--- REACT/reactVite/src/componentes/RedesSociales.jsx
+++ REACT/reactVite/src/componentes/RedesSociales.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import fotoIES from "/headerIES.jpg";
-import {FaFacebookF} from "react-icons/fa";
+import { FaFacebookSquare,FaInstagram } from "react-icons/fa";
+import { DiAndroid } from "react-icons/di";
+
+
 export const RedesSociales = () => {
   return (
     <div>
         <p> Incluir imágenes externas </p>
         <img src="../public/vite.svg" alt="logo de VITE" />
-        <img src={fotoIES} alt="foto INSTITUTO CURA VALERA" />
+        <a href="https://www.iescuravalera.es"><img src={fotoIES} alt="foto INSTITUTO CURA VALERA" /></a>
 
 
         <p> Incluir iconos externos </p>
-        <FaFacebookF />
+       <FaFacebookSquare/> 
+       <a href="https://www.instagram.com/"><FaInstagram/></a> <DiAndroid/>
     </div>
+  )
+}
+
+
+
+export const RedesSociales2 = ({enlaces}) => {
+  let salida="";
+  return (
+    <>
+      <a href={enlaces[0].url}><img src={enlaces[0].imagen} /></a>
+      <a href={enlaces[1].url}><img src={enlaces[1].imagen} /></a>
+    </>
   )
 }

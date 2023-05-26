6057ed1e5e843b1b5f525377b0a10fd66b4b624a
lfvalcar <luisfer998845@gmail.com>
2023-05-26 18:53:40 +0200

    Cambio de proyeccto vite-project a DiapositivasRutas

---
+++ Tema5DesarrolloAvanzadoWeb/DiapositivasRutas/src/componentes/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+export default function NavBar () {
+    return (
+      <nav>
+        <ul>
+          <li><NavLink to='/'><p>Home</p></NavLink></li>
+          <li><NavLink to='/redesSociales'><p>Actividad Redes Sociales</p></NavLink></li>
+          <li><NavLink to='/carousel'><p>Actividad Carousel</p></NavLink></li>
+          <li><NavLink to='/curriculum'><p>Curriculum</p></NavLink>
+            <ul>
+              <li><NavLink to='/curriculum/sobreMi'><p>Sobre Mí</p></NavLink></li>
+              <li><NavLink to='/curriculum/aptitudes'><p>Aptitudes</p></NavLink></li>
+              <li><NavLink to='/curriculum/formacionAcademica'><p>Fomración Académica</p></NavLink></li>
+              <li><NavLink to='/curriculum/formacionProfesional'><p>Formación Laboral</p></NavLink></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
--- Tema5DesarrolloAvanzadoWeb/DiapositivasRutas/src/componentes/Rutas.jsx
+++ Tema5DesarrolloAvanzadoWeb/DiapositivasRutas/src/componentes/Rutas.jsx
@@ -18,17 +18,15 @@ export default function Rutas () {
           <Route exact path="/" element={<HomeR/>} />
             <Route path="/carousel" element={<Carousel/>} />
             <Route path="/redesSociales" element={<RedesSociales/>} />
-            {/* <Route path="/rutas" element={<Rutas/>} />
-              <Route path="/rutas/productos/:nombre" element={<Procesador/>} />
-              <Route path="/rutas/productos/:nombre" element={<Teclado/>} />
-              <Route path="/rutas/productos/:nombre" element={<Ram/>} />
-              <Route path="/rutas/productos/:nombre" element={<PlacaBase/>} />
-              <Route path="/rutas/productos/:nombre" element={<Raton/>} />
-              <Route path="/rutas/curriculum" element={<Curriculum/>} />
-              <Route path="/rutas/curriculum/sobreMi" element={<SobreMi/>} />
-              <Route path="/rutas/curriculum/aptitudes" element={<Aptitudes/>} />
-              <Route path="/rutas/curriculum/formacionAcademica" element={<FormacionAcademica/>} />
-              <Route path="/rutas/curriculum/formacionProfesional" element={<FormacionProfesional/>} /> */}
+              {/* <Route path="/rutas/productos/:nombre" element={<Procesador/>} />
+              <Route path="/productos/:nombre" element={<Teclado/>} />
+              <Route path="/productos/:nombre" element={<Ram/>} />
+              <Route path="/productos/:nombre" element={<PlacaBase/>} />
+              <Route path="/productos/:nombre" element={<Raton/>} /> */}
+              <Route path="/curriculum/sobreMi" element={<SobreMi/>} />
+              <Route path="/curriculum/aptitudes" element={<Aptitudes/>} />
+              <Route path="/curriculum/formacionAcademica" element={<FormacionAcademica/>} />
+              <Route path="/curriculum/formacionLaboral" element={<FormacionLaboral/>} />  
           <Route path="*" element={<NoPage/>} />
         </Routes>
         </main>
@@ -55,7 +53,11 @@ export default function Rutas () {
       ];
 
     return (
-        <CarouselBootstrap andalucia={andalucia} />
+        <div>
+            <h1>Carousel bootstrap en React</h1>
+            <p><strong>Actividad:</strong>Empleando Bootstrap haz un Carousel con imágenes de Andalucía que le indicas en un JSON como parámetro</p>
+            <CarouselBootstrap andalucia={andalucia} />
+        </div>
     );
   }
 
@@ -63,6 +65,22 @@ export default function Rutas () {
     return (
         <IconosRedesSociales />
     );
+  }
+
+  function SobreMi() {
+    return <div>Sobre Mi</div>;
+  }
+
+  function Aptitudes() {
+    return <div>Aptitudes</div>;
+  }
+
+  function FormacionAcademica() {
+    return <div>Formación Académica</div>;
+  }
+
+  function FormacionLaboral() {
+    return <div>Formación Laboral</div>;
   }
 
   function NoPage() {

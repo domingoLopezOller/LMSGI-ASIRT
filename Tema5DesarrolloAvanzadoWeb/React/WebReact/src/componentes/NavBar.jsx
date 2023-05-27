acd8fe4204b5d4ebbc721a0522579f4d32ad45cf
lfvalcar <luisfer998845@gmail.com>
2023-05-27 14:02:08 +0200

    header terminado

--- Tema5DesarrolloAvanzadoWeb/React/WebReact/src/componentes/NavBar.jsx
+++ Tema5DesarrolloAvanzadoWeb/React/WebReact/src/componentes/NavBar.jsx
@@ -15,7 +15,15 @@ const NavBar = () => {
   return (
     <>
     <nav>  
-    
+        <ul>
+            <li><span class="inicio"><a href="#"><i class="fa-solid fa-house"></i><p>Home</p></a></span></li>
+            <li class="seleccionado"><span class="blog"><a href="#"><i class="fa-solid fa-building"></i><p>Company</p></a></span></li>
+            <li><span class="sobremi"><a href="#"><i class="fa-brands fa-servicestack"></i><p>Services</p></a></span></li>
+            <li><span class="fotos"><a href="#"><i class="fa-solid fa-film"></i><p>Showcase</p></a></span></li>
+            <li><span class="portfolio"><a href="#"><i class="fa-solid fa-headset"></i><p>Support</p></a></span></li>
+            <li><span class="contacto"><a href="#"><i class="fa-solid fa-user-secret"></i><p>Privacy</p></a></span></li>
+            <li><span class="enlaces"><a href="#"><i class="fa-solid fa-address-book"></i><p>Contact</p></a></span></li>
+        </ul>
     </nav>
     </>
   )

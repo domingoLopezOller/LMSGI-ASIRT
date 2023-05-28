0a42012cdfd82588951b07f5c1e4f1480080748b
migatomax123 <kevin.acupar18@gmail.com>
2023-05-28 23:58:14 +0200

    cambios

--- pruebashtml/tareas3trimestrefinal/rutas/rutas/src/componentes/NavBar.jsx
+++ pruebashtml/tareas3trimestrefinal/rutas/rutas/src/componentes/NavBar.jsx
@@ -7,8 +7,8 @@ export default function NavBar () {
       <nav>
         <ul>
           <li><NavLink to='/'><p>HOME</p></NavLink></li>
-          <li><NavLink to='/redesSociales'><p>Actividad Redes Sociales</p></NavLink></li>
-          <li><NavLink to='/carousel'><p>Actividad Carousel</p></NavLink></li>
+          <li><NavLink to='/redesSociales'><p>Redes Sociales</p></NavLink></li>
+          <li><NavLink to='/carousel'><p>BOSTRAP</p></NavLink></li>
           <li><NavLink to='sobremi'><p>ABOUT</p></NavLink></li>
         </ul>
       </nav>
